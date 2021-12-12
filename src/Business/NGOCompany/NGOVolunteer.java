6b10cc411207e9da3c5c7ebc3c33d32f6f8896df
Krishna Mayavat <mayavat.k@northeastern.edu>
2021-12-12 04:49:19 -0500

    added java classes foor NGO module

---
+++ src/Business/NGOCompany/NGOVolunteer.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.NGOCompany;
+
+/**
+ *
+ * @author krishnamayavat
+ */
+public class NGOVolunteer {
+    private String Name ;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }   
+}
